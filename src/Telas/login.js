39ca71cee87dd7248b5139d400b3c6ea67874b26
EduardoMG11 <eduardo.gadelha@ort.org.br>
2025-12-18 22:03:44 -0300

    commit login

--- src/Telas/login.js
+++ src/Telas/login.js
@@ -1,3 +1,79 @@
+import { Text, View, StyleSheet } from "react-native";
+import { TextInput } from "react-native";
+import { TouchableOpacity } from "react-native";
+
 export default function TelaLogIn() {
-  return null; // ou qualquer View
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text1}>
+        Bem-vindo de volta. Faça login para continuar.
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        //onPress={() => console.log("Login")}
+      >
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eeeeee",
+    justifyContent: "space-between",
+    paddingVertical: 80,
+    paddingHorizontal: 30,
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 20,
+    fontFamily: "System",
+  },
+  input: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 10,
+    elevation: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    backgroundColor: "#fff",
+    padding: 15,
+  },
+  button: {
+    backgroundColor: "#fff",
+    paddingVertical: 15,
+    marginHorizontal: 5,
+    borderRadius: 12,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 5,
+  },
+  buttonText: {
+    fontFamily: "System",
+    fontSize: 16,
+    color: "#6b6b9a",
+  },
+});
