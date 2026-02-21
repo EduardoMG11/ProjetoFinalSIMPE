bcd2673632267db033c062ab102182d6ae2d8908
EduardoMG11 <eduardo.gadelha@ort.org.br>
2026-02-21 18:19:36 -0300

    ganar la copa: tentando de novo solucionar problema no CNG do EAS

--- app.config.js
+++ app.config.js
@@ -1,21 +1,5 @@
-import fs from "fs";
-import path from "path";
-
 export default ({ config }) => {
-  const googleServicesPath = path.resolve(
-    __dirname,
-    "android/app/google-services.json",
-  );
-
-  if (process.env.GOOGLE_SERVICES_JSON) {
-    fs.mkdirSync(path.dirname(googleServicesPath), { recursive: true });
-    const jsonContent = Buffer.from(
-      process.env.GOOGLE_SERVICES_JSON,
-      "base64",
-    ).toString("utf-8");
-    fs.writeFileSync(googleServicesPath, jsonContent, "utf-8");
-    console.log("✅ google-services.json written to", googleServicesPath);
-  }
-
-  return config;
+  return {
+    ...config,
+  };
 };
